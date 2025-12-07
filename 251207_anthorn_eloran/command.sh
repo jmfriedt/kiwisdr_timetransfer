02f293a502af4d7a5a4a77212474f892b5f138d9
Samuel OLAFUSI <olafusisamuelwealth@gmail.com>
2025-12-07 21:23:34 +0100

    Delete 251207_anthorn_eloran directory

--- 251207_anthorn_eloran/command.sh
+++
@@ -1,1 +0,0 @@
-../kiwiclient/kiwirecorder.py  -d .  --station=kirk_michael  --tlimit=300   -s 21793.proxy.kiwisdr.com -p 8073 -f 100 -w -m iq
--- 251207_anthorn_eloran/command2.sh
+++
@@ -1,1 +0,0 @@
-../kiwiclient/kiwirecorder.py  -d .  --station=G4FUI  --tlimit=300   -s 86.162.54.174 -p 8073 -f 100 -w -m iq
