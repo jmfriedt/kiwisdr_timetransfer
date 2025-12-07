1305466c84542f351f87b01b022b6a61f06b9126
Samuel OLAFUSI <olafusisamuelwealth@gmail.com>
2025-12-07 21:16:52 +0100

    Add files via upload

--- 251207_anthorn_eloran/command.sh
+++ 251207_anthorn_eloran/command.sh
@@ -1,1 +1,1 @@
-../kiwiclient/kiwirecorder.py  -d .  --station=G4FUI  --tlimit=10   -s 86.162.54.174 -p 8073 -f 100 -w -m iq
+../kiwiclient/kiwirecorder.py  -d .  --station=kirk_michael  --tlimit=300   -s 21793.proxy.kiwisdr.com -p 8073 -f 100 -w -m iq
